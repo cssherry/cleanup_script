ff82eed2650bf610ce1e91ae30cffa2b5e577ce6
cssherry <github@cssherry.com>
2021-08-26 20:21:08 +0100

    Add script for cleaning up and writing csv to sqlite db
    - Add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Data cleaning scripts
+
+## Hurricane Best Track Data (HURDAT2)
+
+The publicly available dataset is not properly formatted for importing into db or further analysis. In order to clean up the data for analysis, I did the following:
+
+- Normalize by adding in header row information (alternatively, this could also be moved to different table)
+- Convert date and time fields to ISO8601 format, which is easier for most db and software to interpret
+- Added helper tables that link basins, record identifiers, and statuses to their meanings
+- Normalize longitude/latitude to numbers rather than using E/W, N/S suffixes
+- Mark maximum_wind_knots as null -99
+- Mark -999 wind and pressure values as null so it doesn't interfere with calculations
+- Combine Atlantic and Pacific datasets (Though it it worth noting that the Atlantic dataset has data form 1851, and the Pacific one only has data from 1949)
+
+### To run
+
+`python3 hurricane/process_hurricane.py` will create new hurricane.db sqlite file
+`python3 hurricane/process_hurricane.py <<path_to_sqlite_file>>`
---
+++ hurricane/process_hurricane.py
@@ -0,0 +1,272 @@
+import csv
+import sqlite3
+import sys
+
+# UTILITY FUNCTIONS
+def get_null_or_int(val, maximum=-100):
+    parsed_val = int(val.strip()) # Used to check if val is the "null" placeholder -999
+    return 'NULL' if parsed_val < maximum else str(parsed_val)
+
+def get_string(val):
+    return f"'{val.strip()}'"
+
+def is_numeric(val):
+    try:
+        float(val.strip())
+        return True
+    except ValueError:
+        return False
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Exception as e:
+        print(e)
+
+    return conn
+
+def run_sql(conn, sql):
+    try:
+        c = conn.cursor()
+
+        if ';' in sql:
+            c.executescript(sql)
+        else:
+            c.execute(sql)
+    except Exception as e:
+        print(e)
+
+# 1) Create tables
+def create_table(conn):
+    sql = """
+    -- SET UP INFORMATION TABLES
+
+    -- Define Basins
+    DROP TABLE IF EXISTS hurricane_basins;
+
+    CREATE TABLE hurricane_basins (
+        id text PRIMARY KEY,
+        type text
+    );
+    
+    INSERT INTO hurricane_basins(id, type)
+    VALUES
+        ('AL', 'Atlantic'),
+        ('EP', 'Northeast Pacific'),
+        ('CP', 'North Central Pacific');
+
+    -- Define Record Identifiers
+    DROP TABLE IF EXISTS hurricane_record_identifiers;
+    
+    CREATE TABLE hurricane_record_identifiers (
+        id text PRIMARY KEY,
+        type text
+    );
+
+    INSERT INTO hurricane_record_identifiers (id, type)
+    VALUES
+        ('C', 'Closest approach to a coast, not followed by a landfall'),
+        ('L', 'Landfall (center of system crossing a coastline)  '),
+        ('G', 'Genesis'),
+        ('P', 'Minimum in central pressure  '),
+        ('R', 'Provides additional detail on the intensity of the cyclone when rapid changes are underway'),
+        ('I', 'An intensity peak in terms of both pressure and maximum wind  '),
+        ('S', 'Change of status of the system  '),
+        ('T', 'Provides additional detail on the track (position) of the cyclone');
+
+
+    -- Define Status
+    DROP TABLE IF EXISTS hurricane_status;
+    
+    CREATE TABLE hurricane_status (
+        id text PRIMARY KEY,
+        type text,
+        intensity text
+    );
+
+    INSERT INTO hurricane_status (id, type, intensity)
+    VALUES
+        ('TD', 'Tropical cyclone of tropical depression intensity', '< 34 knots'),
+        ('TS', 'Tropical cyclone of tropical storm intensity', '34-63 knots'),
+        ('HU', 'Tropical cyclone of hurricane intensity', '> 64 knots'),
+        ('EX', 'Extratropical cyclone', 'any intensity'),
+        ('SD', 'Subtropical cyclone of subtropical depression intensity', '< 34 knots'),
+        ('SS', 'Subtropical cyclone of subtropical storm intensity', '> 34 knots'),
+        ('LO', 'A low that is neither a tropical cyclone, a subtropical cyclone, nor an extratropical cyclone', 'any intensity'),
+        ('DB', 'Disturbance', 'any intensity');
+    
+    -- SET UP MAIN TABLE
+    DROP TABLE IF EXISTS hurricane_data;
+
+    CREATE TABLE /* IF NOT EXISTS */ hurricane_data (
+        -- HEADER INFO
+        basin_id text NOT NULL,
+        atcf_cyclone_number_id text NOT NULL,
+        year_id integer NOT NULL,
+        name text NULL,
+        num_entries integer NOT NULL,
+
+        -- RECORD INFO
+        date text NOT NULL,
+        record_identifier text NULL,
+        status text NOT NULL,
+        latitude real NOT NULL,
+        longitude real NOT NULL,
+        maximum_wind_knots integer NULL,
+        minimum_pressure_millibars integer NULL,
+        ne_34_kt_wind_nautical_miles integer NULL,
+        se_34_kt_wind_nautical_miles integer NULL,
+        sw_34_kt_wind_nautical_miles integer NULL,
+        nw_34_kt_wind_nautical_miles integer NULL,
+        ne_50_kt_wind_nautical_miles integer NULL,
+        se_50_kt_wind_nautical_miles integer NULL,
+        sw_50_kt_wind_nautical_miles integer NULL,
+        nw_50_kt_wind_nautical_miles integer NULL,
+        ne_60_kt_wind_nautical_miles integer NULL,
+        se_60_kt_wind_nautical_miles integer NULL,
+        sw_60_kt_wind_nautical_miles integer NULL,
+        nw_60_kt_wind_nautical_miles integer NULL,
+
+        /* CONSTRAINT uniq_cyclone_number UNIQUE (basin_id, atcf_cyclone_number_id, year_id), */
+        CONSTRAINT chk_basin check(basin_id in ('AL', 'EP', 'CP')),
+        CONSTRAINT chk_record check(record_identifier in (NULL, 'C', 'L', 'G', 'P', 'R', 'I', 'S', 'T')),
+        CONSTRAINT chk_status check(status in (NULL, 'TD', 'TS', 'HU', 'EX', 'SD', 'SS', 'LO', 'DB'))/* ,
+        PRIMARY KEY (basin_id, atcf_cyclone_number_id, year_id) */
+    );"""
+
+    run_sql(conn, sql)
+
+# 2) Parse CSV data by flattening sections
+def get_csv_data(file_name):
+    result = []
+    number_rows = 0
+    expected_number_rows = 0
+    header_array = []
+    with open(file_name) as csv_file:
+        hurricane_data = csv.reader(csv_file)
+
+        for row in hurricane_data:
+            # Detect header rows
+            is_not_header = is_numeric(row[0])
+
+            if is_not_header:
+                # HANDLE DATA ROWS
+                len_header = len(header_array)
+                if len_header > 5 or len_header <= 0:
+                    print(f'Bad header: {header_array}')
+                    print(f'Skipping row: {row}')
+                else:
+                    number_rows += 1
+                    # Get date into ISO8601 format: "YYYY-MM-DD HH:MM:SS.SSS"
+                    date = row[0].strip()
+                    time = row[1].strip()
+                    datetime = get_string(f'{date[:4]}-{date[4:6]}-{date[6:]} {time[:2]}:{time[2:]}:00.000')
+
+                    # Get other calculated values
+                    record_raw = row[2].strip()
+                    record = get_string(record_raw) if record_raw else 'NULL'
+
+                    latitude_raw = row[4].strip()
+                    latitude_num = latitude_raw[:-1]
+                    latitude = latitude_num if latitude_raw[-1] == 'N' else f'-{latitude_num}'
+
+                    longitude_raw = row[5].strip()
+                    longitude_num = longitude_raw[:-1]
+                    longitude = longitude_num if longitude_raw[-1] == 'E' else f'-{longitude_num}'
+
+                    maximum_wind_knots = get_null_or_int(row[6], -50) # Filter out -99 for null
+
+                    # Create row
+                    curr_row = [datetime, record, get_string(row[3]), latitude, longitude, maximum_wind_knots]
+
+                    # Get pressure and wind rows (replacing -999 with null)
+                    # Need to filter out empty element at end, but do it in flexible manner in case csv gets fixed
+                    pressure_and_wind = [get_null_or_int(val) for val in filter(lambda val: val.strip(), row[7:])]
+
+                    # Add complete row to results
+                    result.append(header_array + curr_row + pressure_and_wind)
+            else:
+                # HANDLE HEADER ROW
+
+                # Check that the previous rows matched expected
+                if number_rows != expected_number_rows:
+                    print(f'Row number mismatch for {header_array}')
+                    print(f'Expected {expected_number_rows}, but got {number_rows}')
+
+                # Now parse header
+                cyclone_number = row[0].strip()
+                basin = get_string(cyclone_number[:2])
+                atcf = get_string(cyclone_number[2:4])
+                year = cyclone_number[4:]
+
+                name_raw = row[1]
+                name = 'NULL' if name_raw.strip().upper() == 'UNNAMED' else get_string(name_raw)
+
+                expected_number_rows = int(row[2].strip()) # sets expected row as well
+
+                # Generate header cols
+                header_array = [basin, atcf, year, name, str(expected_number_rows)]
+
+                # Reset row count
+                number_rows = 0 
+
+    return result
+
+# 3) Insert into hurricane_data table
+def add_rows(conn, rows):
+    sql = """
+    INSERT INTO hurricane_data (
+        basin_id,
+        atcf_cyclone_number_id,
+        year_id,
+        name,
+        num_entries,
+        date,
+        record_identifier,
+        status,
+        latitude,
+        longitude,
+        maximum_wind_knots,
+        minimum_pressure_millibars,
+        ne_34_kt_wind_nautical_miles,
+        se_34_kt_wind_nautical_miles,
+        sw_34_kt_wind_nautical_miles,
+        nw_34_kt_wind_nautical_miles,
+        ne_50_kt_wind_nautical_miles,
+        se_50_kt_wind_nautical_miles,
+        sw_50_kt_wind_nautical_miles,
+        nw_50_kt_wind_nautical_miles,
+        ne_60_kt_wind_nautical_miles,
+        se_60_kt_wind_nautical_miles,
+        sw_60_kt_wind_nautical_miles,
+        nw_60_kt_wind_nautical_miles
+    ) 
+    VALUES 
+    """
+    
+    parsed_rows = [', '.join(row) for row in rows]
+    all_rows = '), ('.join(parsed_rows)
+    sql += f"({all_rows});"
+    # print(f'Inserting sql: {sql}')
+    run_sql(conn, sql)
+
+if __name__ == "__main__":
+    print('Get connection and create necessary tables')
+    args = sys.argv[1:]
+    table_path = args[0] if len(args) else '~/hurricane.db'
+    conn = create_connection(table_path)
+    create_table(conn)
+
+    print('Get data')
+    data_atlantic = get_csv_data('hurricane/atlantic-hurdat2-1851-2020-052921.csv')
+    data_pacific = get_csv_data('hurricane/pacific-hurdat2-nepac-1949-2020-043021a.csv')
+    
+    print('Add data')
+    add_rows(conn, data_atlantic)
+    add_rows(conn, data_pacific)
